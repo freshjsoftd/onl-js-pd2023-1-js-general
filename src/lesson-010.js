5d8582de76fae6a18725cdbeeedc9487a2e902c9
yan <yan.braverman37@gmail.com>
2023-04-29 13:00:53 +0300

    Parse type

--- src/lesson-010.js
+++ src/lesson-010.js
@@ -1,11 +1,13 @@
 'use strict';
-let number1 = '123.45e';
+/* let number1 = '123.011';
+const user = {}
+const fun = () => {}
 console.log(number1);
-console.log(Number(true));
-console.log(typeof Number(false));
-console.log(typeof number1);
-console.log(NaN**0)
-console.log(isNaN(Number(number1)));
-console.log(isNaN());
-
-// not a number
+// console.log(parseFloat(number1));
+console.log(typeof String(false));
+console.log(Boolean('false')); */
+// console.log(1/-0);
+let number1 = '123';
+console.log(number1 % 20);
+console.log(120 + '12');
+console.log(!!'');

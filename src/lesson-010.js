61f0bffafdc303e24ec2bb77df33915f6b27623a
yan <yan.braverman37@gmail.com>
2023-04-29 13:23:43 +0300

    Test operators

--- src/lesson-010.js
+++ src/lesson-010.js
@@ -1,13 +1,8 @@
 'use strict';
-/* let number1 = '123.011';
-const user = {}
-const fun = () => {}
-console.log(number1);
-// console.log(parseFloat(number1));
-console.log(typeof String(false));
-console.log(Boolean('false')); */
-// console.log(1/-0);
-let number1 = '123';
-console.log(number1 % 20);
-console.log(120 + '12');
-console.log(!!'');
+let hi = 'Hi everybody';
+// alert(hi);
+/* let res = confirm('Do you want to get smth');
+console.log(res); */
+/* let res2 = parseInt(prompt('What is your favorite number?'));
+console.log(typeof res2);
+console.log(res2); */

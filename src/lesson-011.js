270ee1baeff865e677882f92c32b02ddd3633fd6
yan <yan.braverman37@gmail.com>
2023-04-30 14:01:20 +0300

    Finish lesson#11

--- src/lesson-011.js
+++ src/lesson-011.js
@@ -4,19 +4,33 @@
        a должно быть ≤ 100 и > 0.
        Сумму вывести в консоль. 
        Нужна проверка на number. */
-let a = 0;
+/* let a = Number(prompt('Enter a number'));
 let result = 0;
 // debugger
-{
-  let c = 10;
-  console.log('first')
-}
-function getName(){
- let d ='dd';
-}
+if (Number.isNaN(a) || a < 0) {
+	console.log('Enter wrong number');
+} else {
+	for (; a <= 100; a++) {
+		result += a;
+	}
+	console.log(result);
+} */
 
-
-for(; a <= 100; a++) {
-  result += a;
+/* Определить суммарный объем в литрах двенадцати вложенных 
+друг в друга шаров со стенками 
+толщиной 10 мм. Внутренний диаметр внутреннего шара равен 50 см. 
+Принять, что шары 
+вкладываются друг в друга без зазоров. 4/3*PI*rad**3 */
+/* console.log(Math.PI);
+const PI = Math.PI;
+console.log(PI.toPrecision(2)); */
+let res = 0;
+let radius = 0.5/2;
+for(let i = 0; i < 12; i++){
+  const thickness = 0.01;
+  let volume = (4/3*Math.PI*radius**3);
+  radius += thickness;
+  res += volume;
 }
-console.log(result);
+console.log(res.toPrecision(5))
+

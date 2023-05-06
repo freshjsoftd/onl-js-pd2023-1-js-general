e1456d67090bc88fb9d6576b2e024fe08e977ff9
yan <yan.braverman37@gmail.com>
2023-05-06 11:42:08 +0300

    Add test return

--- src/lesson-012-function.js
+++ src/lesson-012-function.js
@@ -1,32 +1,40 @@
 'use strict';
-/* console.log(summ(10, 20));
-// Function Declaration
-function summ(a, b){
-  return a + b;
-}
-console.log(summ(3, 5))
 
-// Function Expression
-const devide = function(a, b){
-  return a / b;
+/* const multTwo = (a) => a * 2;
+console.log(multTwo(50, 70)); */
+
+/* const summ = (a = 20, b = 5) => a / b;
+console.log(summ()); */
+/* function dev(a, b) {
+  return (
+  a / b
+  )
 }
-console.log(devide(10, 20)); */
-let age = 15;
-let welcome;
-if (age < 18) {
-  // welcome();
-	// function welcome() {
-	// 	console.log('You are too much young');
-  welcome = function(){
-    console.log('You are too much young');
-  }
-	// }
-} else {
-	// function welcome() {
-	// 	console.log('You are enough adult');
-	// }
-  welcome = function () {
-		console.log('You are enough adult');
-  }
+console.log(dev(20, 5)); */
+
+/* let brand = 'Stiga';
+let ttBlade = 'Donic';
+function changeArg(brand, ttBlade){
+  brand = 'Butterfly';
+  ttBlade = 'DHS';
+  console.log(brand);
+  console.log(ttBlade);
 }
-welcome();
+
+changeArg(brand, ttBlade);
+
+console.log(brand);
+console.log(ttBlade); */
+
+function checkAge(age = 16) {
+  if(typeof age !== 'number' && !Number.isNaN(age)) {
+    return 'Enter number, please';
+  }
+	if (age < 18) {
+		return 'You are too much young';
+	}
+	if (age >= 18) {
+		return 'You are enough adult';
+	}
+}
+console.log(checkAge(18));

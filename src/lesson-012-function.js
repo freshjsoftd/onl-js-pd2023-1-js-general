414d116c7e6ca7039cb195dc4bab39a2f43dbc40
yan <yan.braverman37@gmail.com>
2023-05-06 13:16:53 +0300

    Add stack

--- src/lesson-012-function.js
+++ src/lesson-012-function.js
@@ -1,27 +1,17 @@
 'use strict';
 
-/* Требуется написать стрелочную функцию, выводящую в консоль числа от 1 до n, 
-        где n — это целое число, которое функция принимает в качестве аргумента, с такими условиями:
-        вывод фразы "fiss" вместо чисел, кратных 3;
-        вывод фразы "buss" вместо чисел, кратных 5;
-        вывод фразы "fiss-buss" вместо чисел, кратных как 3, так и 5. */
-
-/* const printFissBuss = n => {
-	for (let i = 1; i <= n; i++) {
-		if (i % 3 === 0 && i % 5 === 0) {
-			console.log('fiss-buss');
-		}else if(i % 3 === 0){
-      console.log('fiss');
-    }else if (i % 5 === 0) {
-		console.log('buss');
-	} else {
-    console.log(i)
-  }
-	}
-};
-printFissBuss(100); */
-// let numb1 = 10;
-/* function changeGlobal(numb1 = 10) {
-	return numb1 * Math.random();
+// debugger
+/* function greet(person){
+  console.log(`Hello ${person}`)//line #2
 }
-console.log(changeGlobal()) */
+greet('Jhon');
+console.log('Bye-Bye'); */
+// debugger
+/* function chicken(){
+  return egg();
+}
+function egg(){
+  return chicken();
+}
+chicken() */
+// SOLID

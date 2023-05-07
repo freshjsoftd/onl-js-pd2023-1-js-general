38ec1943a7ffee5ca2c90393dbd2ff261bc126dd
yan <yan.braverman37@gmail.com>
2023-05-07 14:02:06 +0300

    After lesson#13 object

--- src/lesson-012-function.js
+++ src/lesson-012-function.js
@@ -24,34 +24,4 @@ summ(10, 20) */
    Разница в возрасте не должна быть меньше 15 лет. Возрат не может быть отрицательным. 
    Проверки на NaN не делаем. 
    Добавить документацию для нашей функции
- */
-/**
- *
- * @param {number} sonAge
- * @param {number} fatherAge
- * @returns {number} year when father was/will be twofold older than son
- */
-const ageTwoFold = (sonAge, fatherAge) => {
-	let result = 0;
-	if (fatherAge - sonAge < 15 || sonAge <= 0) {
-		return 'You entered wrong information';
-	}
-  if(fatherAge / sonAge === 2){
-    return result;
-  }
-  if (fatherAge / sonAge > 2) {
-		while (fatherAge / sonAge !== 2) {
-			fatherAge++;
-			sonAge++;
-			result++;
-		}
-		return result;
-  }
-  while (fatherAge / sonAge !== 2) {
-		fatherAge--;
-		sonAge--;
-		result++;
-  }
-  return -result;
-}
-console.log(ageTwoFold(20, 36));
+*/
--- src/lesson-013-object.js
+++ src/lesson-013-object.js
@@ -1,5 +1,11 @@
-'use strict';
+// 'use strict';
 let address = 'Dnipro';
+const sayHi = () => {
+	return this;
+};
+function callBack(sayHi) {
+	return this;
+}
 const user = {
 	fName: 'Jhon',
 	lName: 'Doe',
@@ -10,72 +16,62 @@ const user = {
 	// say: function(){
 	//   console.log('Hi everybody')
 	// }
-	say() {
-		console.log('Hi everybody');
-	},
+	/* say() {
+		console.log(`Hi ${this.fName + this.lName}`);
+	}, */
+	say: sayHi,
+	// hi: () => console.log(`Hi ${this.fName + this.lName}`),
 };
-console.log(user);
+const person = {
+	fName: 'Bill',
+	lName: 'Doe',
+	age: 30,
+	address,
+	isMarried: true,
+	'home address': 'hhh',
+	say: sayHi,
+};
+console.log(user.say());
+console.log(person.say());
+console.log(sayHi());
+// user.hi();
+
 // ====================
-function setTitle(rate, lastName) {
-	let title;
-	if (rate >= 60) {
-		title = 'Мастер';
-	} else if (rate >= 40) {
-		title = 'КМС';
-	} else {
-		title = 'Разряд';
-	}
-	const sportsman = {
-		[title]: rate,
-		lastName,
+/* const car2 = {
+	brand: 'Toyota',
+	model: 'Avalon',
+	'year of relise': 1995,
+	transmition: 'mechanics',
+	// '10': 10,
+	isWell: true,
+	color: 'green',
+	go: driver =>
+		console.log(`Let's go ${driver} on ${this.brand} ${this.model}`),
+	run: function (driver) {
+		console.log(`Let's drive ${driver} on ${this.brand} ${this.model}`);
+	},
+	// run(driver) {
+	// 	console.log(`Let's drive ${driver} on ${this.brand} ${this.model}`);
+	// },
+}; */
+
+// car2.run('Jhon')
+// car2.go('Jhon')
+function Car(brand, model, transmition, color) {
+	// this = {};
+	this.brand = brand;
+	this.model = model;
+	this.transmition = transmition;
+	this.color = color;
+	this.fullBrand = `${brand} ${model}`;
+	this.run = function (driver) {
+		console.log(`Let driver ${driver} on ${this.fullBrand}`);
 	};
-	console.log(title in sportsman);
-	return sportsman;
 }
-console.log(setTitle(30, 'Jhon'));
-user.say();
 
-/* Создайте объект car со свойствами: brand, model, year of relise,
- transmition, isWell, color, run()
-   run() создать при помощи FunctionDeclaration
- */
-// console.log('home' in user);
-for (const item in user) {
-	console.log(`${item}: ${user[item]}`);
-}
-/* Напишите функцию isEmpty(obj), которая возвращает true,
- если у объекта нет свойств, иначе false. */
-
- const obj = {};
- function isEmpty(obj) {
-		for (let key in obj) {
-			return false;
-		}
-		return true;
- }
- console.log(isEmpty(user));
-
- /* Создайте функцию multiplyNum(obj), которая умножает 
-    все числовые свойства объекта price на 2 и 
-    затем выведите в консоле список такого вида key: value. 
-    Объект price - счет в ресторане, 5 
-    числовых свойств, одно boolean и одно string */
-    const price = {
-		tea: 20,
-		coffee: 30,
-		meal: 50,
-		onion: 10,
-		waiter: 'Jhon Doe',
-		isPaid: false,
-	};
-  function multBill(bill){
-    for (const key in bill) {
-      if(typeof bill[key] === 'number'){
-        bill[key] = bill[key] * 2;
-      }
-    }
-  }
-  multBill(price);
-  for(const key in price){
-    console.log(`${key} : ${price[key]}`)
-  }
+const bmw = new Car('BMW', 'X5', 'automatic', 'grey');
+const toyota = new Car('Toyota', 'Camry', 'mechanic', 'red');
+console.log(bmw.fullBrand);
+console.log(bmw);
+bmw.run('Jhon');
+toyota.run('Jane');

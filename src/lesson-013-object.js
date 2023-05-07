e0b6b19933d2ea44150f1f750f9889e166a922b0
yan <yan.braverman37@gmail.com>
2023-05-07 11:04:48 +0300

    Add calculate properties

--- src/lesson-013-object.js
+++ src/lesson-013-object.js
@@ -1,29 +1,40 @@
 'use strict';
+let address = 'Dnipro';
 const user = {
 	fName: 'Jhon',
 	lName: 'Doe',
 	age: 30,
-	address: {
-	  country: 'United States',
-	  city: 'NY',
-	},
+	address,
 	isMarried: true,
 	'home address': 'hhh',
-	0: 'ffv',
+	// say: function(){
+	//   console.log('Hi everybody')
+	// }
+	say() {
+		console.log('Hi everybody');
+	},
 };
 console.log(user);
-// const person = new Object(user);
-const person = user;
-console.log(person);
-console.log(user === person);
-console.log(user.age);
-console.log(user['home address']);
-person.age = 40;
-delete person.address;
-console.log(person);
-console.log(user);
-// CRUD
-// C - create
-// R - read
-// U - update
-// D - delete
+// ====================
+function setTitle(rate, lastName) {
+	let title;
+	if (rate >= 60) {
+		title = 'Мастер';
+	} else if (rate >= 40) {
+		title = 'КМС';
+	} else {
+		title = 'Разряд';
+	}
+	const sportsman = {
+		[title]: rate,
+		lastName,
+	};
+	return sportsman;
+}
+console.log(setTitle(30, 'Jhon'));
+user.say();
+
+/* Создайте объект car со свойствами: brand, model, year of relise,
+ transmition, isWell, color, run()
+   run() создать при помощи FunctionDeclaration
+ */

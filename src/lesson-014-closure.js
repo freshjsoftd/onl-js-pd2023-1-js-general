9eeac2cc304b97c4b1b636562f6382cdabc924aa
yan <yan.braverman37@gmail.com>
2023-05-13 12:29:01 +0300

    Add recursion

--- src/lesson-014-closure.js
+++ src/lesson-014-closure.js
@@ -1,9 +1,78 @@
 'use strict';
 // debugger
-function fibonacci(position){
-  if(position < 3){
-    return 1
-  }
-  return fibonacci(position - 1) + fibonacci(position - 2);
+const companyEmailAddresses = {
+	finance: ['jill@companyx.com', 'frank@companyx.com'],
+	engineering: {
+		qa: ['ahmed@companyx.com', 'taylor@companyx.com'],
+		development: [
+			'cletus@companyx.com',
+			'bojangles@companyx.com',
+			'bibi@companyx.com',
+		],
+	},
+	management: {
+		directors: [
+			'tanya@companyx.com',
+			'odell@companyx.com',
+			'amin@companyx.com',
+		],
+		projectLeaders: [
+			'bobby@companyx.com',
+			'jack@companyx.com',
+			'harry@companyx.com',
+			'oscar@companyx.com',
+		],
+		hr: ['mo@companyx.com', 'jag@companyx.com', 'ilaria@companyx.com'],
+	},
+	sales: {
+		business: {
+			senior: ['larry@companyx.com', 'sally@companyx.com'],
+		},
+		client: {
+			senior: ['jola@companyx.com', 'amit@companyx.com'],
+			exec: ['asha@companyx.com', 'megan@companyx.com'],
+		},
+	},
+};
+
+function sendEmail(emailAddress) {
+	console.log(`sending email to ${emailAddress}`);
 }
-console.log(fibonacci(10));
+
+/* function gatherEmailAddresses(departments) {
+	let departmentKeys = Object.keys(departments);
+	for (let i = 0; i < departmentKeys.length; i++) {
+		if (Array.isArray(departments[departmentKeys[i]])) {
+			departments[departmentKeys[i]].forEach(email => sendEmail(email));
+		} else {
+			for (let dept in departments[departmentKeys[i]]) {
+				if (Array.isArray(departments[departmentKeys[i]][dept])) {
+					departments[departmentKeys[i]][dept].forEach(email =>
+						sendEmail(email)
+					);
+				} else {
+					for (let subDept in departments[departmentKeys[i]][dept])
+						if (
+							Array.isArray(
+								departments[departmentKeys[i]][dept][subDept]
+							)
+						) {
+							departments[departmentKeys[i]][dept][
+								subDept
+							].forEach(email => sendEmail(email));
+						}
+				}
+			}
+		}
+	}
+} */
+function gatherEmailAddresses(departments) {
+	let departmentKeys = Object.keys(departments);
+	departmentKeys.forEach(dept => {
+		if (Array.isArray(departments[dept])) {
+			return departments[dept].forEach(email => sendEmail(email));
+		}
+		return gatherEmailAddresses(departments[dept]);
+	});
+}
+gatherEmailAddresses(companyEmailAddresses);

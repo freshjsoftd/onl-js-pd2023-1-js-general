57f63605fccd2371340654612c55a45ccf2ac7a2
yan <yan.braverman37@gmail.com>
2023-05-14 11:53:00 +0300

    Add test primitives methods

--- src/lesson-015-array.js
+++ src/lesson-015-array.js
@@ -1,20 +1,33 @@
 'use strict';
-// Math
-console.log(Math.abs(-20));
-console.log(Math.ceil(20.005));
-console.log(Math.floor(20.999));
-console.log(Math.round(20.45));
-console.log(Math.max());
-console.log(Math.min(20, 30, 1));
-console.log(Math.pow(2, 64));
-console.log(Math.random());
-function getRandom(min, max){
-  return Math.round(Math.random() * (max - min) + min);
-}
-console.log(getRandom(10, 100));
-// Date
-console.log(new Date());
-console.log(new Date().toLocaleDateString());
-console.log(Date.now());
-console.log(Date.now());
-console.log(Date.parse('1985-11-05'));
+/* let numb = 3025512.55822547444;
+console.log(Number('12'));
+console.log(Date.now().toExponential(2));
+console.log(numb.toPrecision(6)); */
+// String
+let sams = '  Samsung Galaxy  ';
+let xiomi = 'Mi Pro 11T';
+let together = sams.concat(' ', xiomi, ' Nokia');
+let sliceStr = together.slice(3, 10);
+let url = 'http://localhost:5501/';
+let arrStr = together.split('', 20)
+let subStr = together.substring(3, 8)
+let samsGap = '  Samsung Galaxy  ';
+console.log(sams.length);
+console.log(sams[6]);
+console.log(sams.charAt(6));
+console.log(sams.charCodeAt(6));
+console.log(together);
+console.log(together.includes('Nok', 20));
+console.log(together.indexOf('o'));
+console.log(together.lastIndexOf('o'));
+console.log(sams.repeat(3));
+console.log(sliceStr);
+console.log(url.slice(url.indexOf('/')+2, url.lastIndexOf(':')));
+console.log(arrStr)
+console.log(subStr);
+console.log(together);
+console.log(together.toUpperCase());
+console.log(together.toLowerCase());
+console.log(together);
+console.log(samsGap.trim());
+

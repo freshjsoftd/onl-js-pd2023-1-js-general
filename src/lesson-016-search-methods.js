fc5caa7e604c14a70eebbe49ae2d7e3c98f5f05a
yan <yan.braverman37@gmail.com>
2023-05-20 14:07:24 +0300

    Add my own implementation of array's search methods

--- src/lesson-016-search-methods.js
+++ src/lesson-016-search-methods.js
@@ -1,52 +1,65 @@
 'use strict';
-// find, findIndex
-const arr1 = [1, 2, 4, 8, 11, 15];
-const arr2 = ['Donic', 'Stiga', 'DHS', 'TSP'];
-const arr3 = [{ name: 'Bob' }, { name: 'Jhon' }, { name: 'Bill' }];
-// console.log(arr1.find((el) => el % 3 === 0))
-// console.log(arr3.find(el => el.name === 'Bill'));
-
-// console.log(arr1.findLastIndex((el) => el % 2 === 0))
-// forEach
-// arr2.forEach(el => console.log(el));
-/* arr1.forEach((el, index, arr) => {
-  arr[index] = el * 10;
-}) */
-// console.log(arr1);
-// map
-const arr4 = ['1', '2', '3', '4', '5', '6'];
-
-function parseToInt(el){
-  return parseInt(el)
+// -----------forEach ----------
+function myForEach(arr, callback, thisArg) {
+	for (let i = 0; i < arr.length; i++) {
+		callback.call(thisArg, arr[i], i, arr);
+	}
 }
-const intArr = arr4.map(el => el);
-// const arr10 = intArr;
-// console.log(arr10 === intArr);
-// console.log(arr4 === intArr);
-// console.log(intArr);
-// console.log(arr2.map((el, i) => `${el} is #${i+1} in the world`));
-// some-every
-// console.log(arr1.every(el => typeof el === 'number'));
-// console.log(arr1.some(el => el % 2 === 0));
-// filter
-const filteredArr = arr3.filter(el => el.name == 'Jhon');
-// console.log(filteredArr);
-// reduce
-/* 
-array.reduce(callback, [initialValue]);
-callback(accum, currentValue, i, arr)
-if(initialValue){
-  accum = initialValue;
-  currentValue = arr[0];
-}else{
-  accum = arr[0];
-  currentValue = arr[1]
+const arrBrand = ['Donic', 'Stiga', 'DHS', 'Xiom'];
+myForEach(arrBrand, (el, i) => console.log(`${el} is number ${i + 1}`));
+// --------------map-------------------
+function myMap(arr, callback, thisArg) {
+	const result = [];
+	for (let i = 0; i < arr.length; i++) {
+		result.push(callback.call(thisArg, arr[i], i, arr));
+	}
+	return result;
 }
- */
-console.log(arr1.reduce((a, b) => a + b))
-const arrArr = [
+const numbArray = [10, 20, 30];
+console.log(myMap(numbArray, el => el / 2));
+// ------------some--------------------------------
+function mySome(arr, callback, thisArg) {
+	for (let i = 0; i < arr.length; i++) {
+		if (callback.call(thisArg, arr[i], i, arr)) {
+			return true;
+		}
+	}
+	return false;
+}
+// ---------------filter --------------------
+function myFilter(arr, callback, thisArg) {
+	const result = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (callback.call(thisArg, arr[i], i, arr)) {
+			result.push(arr[i]);
+		}
+	}
+	return result;
+}
+console.log(myFilter(numbArray, el => el % 3 === 0));
+// -------------reduce-------------------
+function myReduce(arr, callback, startValue) {
+	let result;
+  let index = arr.length - 1;
+	if (startValue) {
+		result = startValue;
+		for (let i = 0; i < arr.length; i++) {
+			result = callback(result, arr[i], i, arr);
+		}
+	} else {
+		result = arr[0];
+		for (let i = 0; i < arr.length-1; i++) {
+			result = callback(result, arr[i + 1], i, arr);
+		}
+	}
+	return result;
+}
+const arrArr2 = [
 	[0, 1],
 	[2, 3],
 	[4, 5],
 ];
-console.log(arrArr.reduce((a, b) => a.concat(b)));
+// numbArray;
+
+console.log(myReduce(arrArr2, (a, b) => a.concat(b)));
+console.log(myReduce(numbArray, (a, b) => a + b));
